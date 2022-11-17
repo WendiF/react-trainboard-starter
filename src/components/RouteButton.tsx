25593e6ef5154845e12a1d406808628ef1a62712
Muh90 <muhanad.al-khafaji@softwire.com>
2022-11-17 11:26:21 +0000

    Implemented hyperlink to take to relevant route page based on selected stations.

--- src/components/RouteButton.tsx
+++ src/components/RouteButton.tsx
@@ -1,22 +1,21 @@
-import React, { createContext, useContext, useEffect, useState } from 'react';
+import React from 'react';
 import { myContext } from './ContextProvider';
-import StationSelection from './StationSelection';
 
 const RouteButton: React.FC<unknown> = () => {
     const { departure, arrival } = React.useContext(myContext);
+    const hyperlink = "https://www.lner.co.uk/travel-information/travelling-now/live-train-times/depart/"
+    + departure.code + "/" + arrival.code + "/#LiveDepResults";
 
     return (
-        
         <div>
-            <a >
-                <button onClick = { (e) => getStations(e, departure, arrival) } type = "button">Click</button>
+            <a href={hyperlink}>
+                <button onClick = { (e) => getStations(e, departure.code, arrival.code) } type = "button">Click</button>
             </a>
         </div>
     );
 };
 
 const getStations = (e: any, departure: string, arrival: string) => {
-
     console.log(departure, arrival);
 };
 export default RouteButton;
