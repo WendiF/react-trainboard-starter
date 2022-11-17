14fb37272e98365a14b52e692ab2259fd1b2bf75
Chiara Gandolfi <Chiara.Gandolfi@softwire.com>
2022-11-17 11:28:38 +0000

    remove unused function

--- src/components/RouteButton.tsx
+++ src/components/RouteButton.tsx
@@ -3,19 +3,16 @@ import { myContext } from './ContextProvider';
 
 const RouteButton: React.FC<unknown> = () => {
     const { departure, arrival } = React.useContext(myContext);
-    const hyperlink = "https://www.lner.co.uk/travel-information/travelling-now/live-train-times/depart/"
-    + departure.code + "/" + arrival.code + "/#LiveDepResults";
+    const hyperlink = 'https://www.lner.co.uk/travel-information/travelling-now/live-train-times/depart/'
+    + departure.code + '/' + arrival.code + '/#LiveDepResults';
 
     return (
         <div>
-            <a href={hyperlink}>
-                <button onClick = { (e) => getStations(e, departure.code, arrival.code) } type = "button">Click</button>
+            <a href = { hyperlink }>
+                <button type = "button">Click</button>
             </a>
         </div>
     );
 };
 
-const getStations = (e: any, departure: string, arrival: string) => {
-    console.log(departure, arrival);
-};
 export default RouteButton;
