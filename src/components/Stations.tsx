d796b3859e661ad168879a324c2e30609e63ba31
Felix Waller <felix.waller@softwire.com>
2023-07-04 14:16:29 +0100

    Add basic station selection

--- src/components/Stations.tsx
+++ src/components/Stations.tsx
@@ -2,8 +2,21 @@ import React, { useEffect, useState } from 'react';
 import { fetchStations } from '../helpers/ApiCallHelper';
 
 const Stations: React.FC = () => {
+    const [allStations, setAllStations] = useState(['Leeds', 'Paddington', 'King\'s Cross', 'Manchester Piccadilly', 'Brighton']);
+    const [allStationCodes, setAllStationCodes] = useState(new Map([
+        ['Leeds', 'LDS'],
+        ['Paddington', 'PAD'],
+        ['King\'s Cross', 'KGX'],
+        ['Manchester Piccadilly', 'MAN'],
+        ['Brighton', 'BTN'],
+    ]));
 
-    const [allStations, setAllStations] = useState([]);
+    const [from, setFrom] = useState('LDS');
+    const [to, setTo] = useState('LDS');
+
+    const handleClick = () => {
+        window.location.href = `https://www.lner.co.uk/travel-information/travelling-now/live-train-times/depart/${from}/${to}/#LiveDepResults`;
+    };
 
     useEffect(() => {
         fetchStations()
@@ -15,9 +28,19 @@ const Stations: React.FC = () => {
     console.log('hello world');
 
     return (
-        <div>
-            Stations!
-        </div>
+        <>
+            <label htmlFor = "from">From:</label>
+            <select name = "from" id = "from" value = { from } onChange = { (event) => setFrom(event.target.value) }>
+                {allStations.map((station) => <option value = { allStationCodes.get(station) } key = { station }>{station}</option>)}
+            </select>
+
+            <label htmlFor = "to">To:</label>
+            <select name = "to" id = "to" value = { to } onChange = { (event) => setTo(event.target.value) }>
+                {allStations.map((station) => <option value = { allStationCodes.get(station) } key = { station }>{station}</option>)}
+            </select>
+
+            <button type = "button" onClick = { handleClick }>Submit</button>
+        </>
     );
 };
 
